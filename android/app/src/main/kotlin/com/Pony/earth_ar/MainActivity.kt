a61219ace6e9a32541ec627d58b8e4314ad3fe44
Ponhrith <ponhrith.nhep@gmail.com>
2023-08-16 12:35:00 +0700

    Succussfully Working

---
+++ android/app/src/main/kotlin/com/Pony/earth_ar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Pony.earth_ar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:earth_ar/ar_earth_map_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter AR Earth Map App',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ArEarthMapScreen(),
+    );
+  }
+}
+
+
